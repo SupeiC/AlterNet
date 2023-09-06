679d7377860f9a659061a37c027d9ea3bdeef163
SupeiC <63715051+SupeiC@users.noreply.github.com>
2023-09-06 09:29:04 +0800

    code uploaded

---
+++ PythonSample/Sample/bin/Debug/Code.py
@@ -0,0 +1,11 @@
+import string
+import sys
+import os
+
+def ex1(input1, input2, input3):
+    x = float(input1);
+    y = float(input2);
+    print("print ADD(x, y): " + str(x + y) + "\n");
+    print("print SUB(x, y): " + str(x - y) + "\n");
+    input3("input3 ADD(x, y): " + str(x + y) + "\n");
+    input3("input3 SUB(x, y): " + str(x - y) + "\n");
