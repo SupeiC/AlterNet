c5fab7e7fda50d49937659fcbb9e1066c1db4f63
Su-Pei CHENG <su-pei.cheng@st.com>
2024-05-20 23:07:02 +0800

    Add PythonDebugger

---
+++ PythonDebugger/DebuggerExample/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿using Alternet.Scripter.Debugger;
+using Alternet.Scripter.Debugger.Python;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DebuggerExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool _isRunning, _isRunningStopped;
+        int _logMessageCount;
+        ExecutionPosition _executionPosition;
+        PythonScriptRunner _pythonScriptRunner;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _pythonScriptRunner = new PythonScriptRunner();
+            if (!_pythonScriptRunner.IsEnabled)
+            {
+                MessageBox.Show("Python is not installed", "Wraning", MessageBoxButton.OK);
+                Environment.Exit(0);
+            }
+
+            SetRunning(false);
+            SetRunningStopped(false);
+            debugEditor.Text = _pythonScriptRunner.GetPythonExample();
+            debugEditor.Lexer = _pythonScriptRunner.GetLexer();
+            debugEditor.Debugger = RenewDebugBreakPoints(null);
+        }
+
+        #region Toolbar
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_isRunning)
+                {
+                    debugEditor.Debugger.Continue();
+                }
+                else
+                {
+                    _logMessageCount = 0;
+                    SetRunning(true);
+                    SetRunningStopped(false);
+
+                    debugEditor.Debugger = RenewDebugBreakPoints(debugEditor.Debugger.Breakpoints.Breakpoints.ToArray());
+                    debugEditor.Debugger.StartDebugging(new StartDebuggingOptions()
+                    {
+                        BreakOnStart = true,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Run ex: " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private async void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await debugEditor.Debugger.StopDebuggingAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("DebugStop ex: " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnStepInto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                debugEditor.Debugger.StepInto();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("StepOver ex: " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnStepOver_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                debugEditor.Debugger.StepOver();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("StepOver ex: " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnStepOut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                debugEditor.Debugger.StepOut();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("StepOver ex: " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void SetRunning(bool running)
+        {
+            _isRunning = running;
+        }
+
+        private void SetRunningStopped(bool stopped)
+        {
+            _isRunningStopped = stopped;
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                btnStop.IsEnabled = stopped;
+                btnStepOver.IsEnabled = stopped;
+                btnStepInto.IsEnabled = stopped;
+                btnStepOut.IsEnabled = stopped;
+            });
+        }
+
+        #endregion
+
+        #region Debugger
+
+        private ScriptDebugger RenewDebugBreakPoints(Breakpoint[] points)
+        {
+            var debugger = _pythonScriptRunner.GetScriptDebugger(debugEditor.Text);
+            debugger.StateChanged += Debugger_StateChanged;
+            debugger.ExecutionStopped += Debugger_ExecutionStopped;
+            debugger.DebuggingStopped += Debugger_DebuggingStopped;
+            debugger.LogMessageReceived += Debugger_LogMessageReceived;
+
+            foreach (var point in points ?? new Breakpoint[] { })
+                debugger.Breakpoints.AddBreakpoint(point.FilePath, point.LineNumber);
+
+            return debugger;
+        }
+
+        private void Debugger_LogMessageReceived(object sender, LogMessageReceivedEventArgs e)
+        {
+            _logMessageCount++;
+            if (_logMessageCount % 2 == 0)
+                return;
+
+            var message = e.Message.Substring(0, e.Message.Length - 1);
+            Trace.WriteLine("[Debugger_LogMessageReceived]" + message);
+            MessageBox.Show(message, "LogMessageReceived", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Debugger_DebuggingStopped(object sender, DebuggingStoppedEventArgs e)
+        {
+            Trace.WriteLine("[Debugger_DebuggingStopped]" + e.ToString());
+            SetRunning(false);
+            ClearExecutionPosition();
+        }
+
+        private void Debugger_ExecutionStopped(object sender, ExecutionStoppedEventArgs e)
+        {
+            Trace.WriteLine($"[Debugger_ExecutionStopped] {e.StopReason}/{e.Position.Line}/{e.Position.File}");
+            switch (e.StopReason)
+            {
+                case ExecutionStopReason.BreakpointHit:
+                    SetRunningStopped(true);
+                    _executionPosition = e.Position;
+                    Application.Current.Dispatcher.Invoke(() => debugEditor.ExecutionStopped(_executionPosition));
+                    break;
+                case ExecutionStopReason.Exception:
+                case ExecutionStopReason.UnhandledException:
+                    Trace.WriteLine($"\t{e.Exception.Message}");
+                    break;
+            }
+        }
+
+        private void Debugger_StateChanged(object sender, DebuggerStateChangedEventArgs e)
+        {
+            Trace.WriteLine($"[Debugger_StateChanged] old/new state: {e.OldState}/{e.NewState}");
+        }
+
+        private void ClearExecutionPosition()
+        {
+            if (_executionPosition != null)
+            {
+                debugEditor.ClearDebugStyles(_executionPosition);
+                _executionPosition = null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ PythonDebugger/DebuggerExample/PythonScriptRunner.cs
@@ -0,0 +1,152 @@
+﻿using Alternet.Scripter.Debugger.Python;
+using Alternet.Scripter.Python;
+using Alternet.Syntax.Lexer;
+using Alternet.Syntax.Parsers.Python;
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggerExample
+{
+    public class PythonScriptRunner
+    {
+        public bool IsEnabled { get; private set; }
+        public string PathToDLL { get; private set; }
+
+        public PythonScriptRunner()
+        {
+            IsEnabled = CheckIfPythonInstalled();
+            if (IsEnabled)
+            {
+                PythonInit();
+            }
+        }
+
+        public string GetPythonExample()
+        {
+            return
+@"
+import string
+import sys
+import os
+
+print(""Hello"")
+x = 2
+y = 3
+print(""x + y = "" + str(x + y))
+";
+        }
+
+        public bool CheckMethodName(string text, string methodName)
+        {
+            string[] lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("def"))
+                {
+                    if (methodName == lines[i].Split(new string[] { "def" }, StringSplitOptions.None)[1].Split('(')[0].Trim())
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        public PythonNETParser GetLexer()
+        {
+            var parser = new PythonNETParser();
+            parser.CodeEnvironment = GetScriptRunner(string.Empty).CodeEnvironment;
+            return parser;
+        }
+
+        public ScriptDebugger GetScriptDebugger(string text)
+        {
+            return new ScriptDebugger()
+            {
+                ScriptRun = GetScriptRunner(text),
+            };
+        }
+
+        private ScriptRun GetScriptRunner(string text)
+        {
+            var scriptRunner = new ScriptRun();
+            scriptRunner.ScriptSource.FromScriptCode(text);
+            scriptRunner.ScriptHost.SkipSyntaxErrorCheck = true;
+            scriptRunner.ScriptSource.ReferencedFrameworks = Alternet.Common.DotNet.Framework.Wpf;
+            scriptRunner.ScriptSource.Imports.Add("System");
+            scriptRunner.ScriptSource.Imports.Add("System.Diagnostics");
+
+            return scriptRunner;
+        }
+
+        private bool CheckIfPythonInstalled()
+        {
+            try
+            {
+                var path = RunProcess("/C python -c \"import os, sys; print(os.path.dirname(sys.executable))\"");
+                if (!string.IsNullOrEmpty(path))
+                {
+                    path = Path.Combine(path, new DirectoryInfo(path).Name.ToLower() + ".dll");
+                    if (File.Exists(path))
+                    {
+                        PathToDLL = path;
+                        return true;
+                    }
+                }
+
+                path = RunProcess("/C py -0p");
+                if (!string.IsNullOrEmpty(path))
+                {
+                    path = path.Substring(path.IndexOf(":") - 1, (path.IndexOf(".exe") + 4) - (path.IndexOf(":") - 1));
+                    path = Path.GetDirectoryName(path);
+                    path = Path.Combine(path, new DirectoryInfo(path).Name.ToLower() + ".dll");
+                    if (File.Exists(path))
+                    {
+                        PathToDLL = path;
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("CheckIfPythonInstalled, ex: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void PythonInit()
+        {
+            if (PythonEngine.IsInitialized)
+            {
+                ScriptEngine.ShutDownRuntime();
+                PythonEngine.Shutdown();
+            }
+
+            Runtime.PythonDLL = PathToDLL;
+            PythonEngine.Initialize();
+            PythonEngine.BeginAllowThreads();
+        }
+
+        private string RunProcess(string arguments)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = arguments;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+
+            proc.Start();
+            proc.WaitForExit();
+
+            return proc.StandardOutput.ReadToEnd().Trim();
+        }
+    }
+}
